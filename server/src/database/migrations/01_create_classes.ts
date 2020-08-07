115047ff30de9680e540902b3c7401c5f46af749
FilippePimentel <f.pimentel@unigranrio.br>
2020-08-07 18:47:36 -0300

    Segundo dia criação da API

---
+++ server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+//Migrations API documentação
+export async function up(knex: Knex) {
+    return knex.schema.createTable('classes', table => {
+        table.increments('id').primary();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
+
+        table.integer('user_id').notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('classes');
+}
